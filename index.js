7c31c633d27e59f25a91f3cca9aefc617f3886f3
gael <76548719+gaelzarco@users.noreply.github.com>
2023-07-03 15:50:34 -0700

    having issues connecting to ws

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const {
     PROXY_ENDPOINT,
-    PROXY_USER,
+    PROXY_USERNAME,
     PROXY_PASSWORD
 } = process.env;
 
@@ -12,9 +12,27 @@ async function run() {
     let browser;
 
     try {
+        // 'brd-customer-<ACCOUNT ID>-zone-<ZONE NAME>:<PASSWORD>'
+        const auth = `${PROXY_USERNAME}:${PROXY_PASSWORD}`
+
         browser = await puppeteer.connect({
-            browserWSEndpoint: PROXY_ENDPOINT
+            browserWSEndpoint: `wss://${auth}@${PROXY_ENDPOINT}`
         })
+
+        const page = await browser.newPage();
+        page.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+        await page.goto('https://amazon.com')
+
+        // const body = await page.$('body')
+
+        const html = await page.evaluate(() => {
+            document.documentElement.outerHTML
+        })
+
+        console.log(html)
+
+        return
     } catch (err) {
         console.error("Scrape failed: ", err)
     } finally {
@@ -22,3 +40,5 @@ async function run() {
     }
 
 }
+
+run()
